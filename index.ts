d8b64f6f27a82b3a01c42dbe1c1e42404962a50c
marco0212 <marco@dwhale.kr>
2024-01-21 14:32:29 +0900

    feat: initialize express server

--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello via Bun!");
+import express from "express";
+
+const app = express();
+const port = 8080;
+
+app.get("/", (req, res) => {
+  // send a simple json response
+  res.json({ message: "Hello Fucking World!" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
